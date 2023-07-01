270e72951e76bc767fe0976580221d5ae6ffdbf2
tfkcoder <lucianojackson048@gmail.com>
2023-07-02 01:27:14 +0300

    update with material 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,7 @@ import '../widget/search_screen_widget.dart';
 import '../widget/transaction_screen_widget.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key ?key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -34,63 +34,73 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Styles.darkColor,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.shopping_bag_outlined,
-              color: Styles.textColor,
-            ),
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          Container(
-            margin: const EdgeInsets.only(right: 24.0),
-            child: CircleAvatar(
-              backgroundColor: Styles.btnColor,
-              backgroundImage: const AssetImage("assets/images/profile.png"),
-            ),
-          )
-        ],
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
+    return Container(
+      color: Styles.blackColor,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Styles.darkColor,
+          actions: [
+            IconButton(
+              onPressed: () {},
               icon: Icon(
-                Icons.menu,
+                Icons.shopping_bag_outlined,
                 color: Styles.textColor,
               ),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Container(
+              margin: const EdgeInsets.only(right: 24.0),
+              child: CircleAvatar(
+                backgroundColor: Styles.btnColor,
+                backgroundImage: const AssetImage("assets/images/profile.png"),
+              ),
+            )
+          ],
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: Icon(
+                  Icons.menu,
+                  color: Styles.textColor,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+            },
+          ),
         ),
+        drawer: Drawer(
+          backgroundColor: Styles.darkColor,
+          width: MediaQuery.of(context).size.width * 0.8,
+        ),
+        body: Container(
+          color: Styles.blackColor,
+          child: TabBarView(
+            controller: _tabController,
+            children: const <Widget>[
+              HomeScreenWidget(),
+              SearchScreenWidget(),
+              FavouriteScreenWidget(),
+              TransactionScreenWidget(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: _buildCustomBottomNavigationBar(),
       ),
-      drawer: Drawer(
-        backgroundColor: Styles.darkColor,
-        width: MediaQuery.of(context).size.width * 0.8,
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const <Widget>[
-          HomeScreenWidget(),
-          SearchScreenWidget(),
-          FavouriteScreenWidget(),
-          TransactionScreenWidget(),
-        ],
-      ),
-      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
-  Widget _buildBottomNavigationBar() {
+  Widget _buildCustomBottomNavigationBar() {
     return Container(
       decoration: BoxDecoration(
-        color: Styles.cardColor,
+        color: Styles.darkColor,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.4),
@@ -98,37 +108,53 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             offset: const Offset(0, -6),
           ),
         ],
+      ),
+      child: ClipRRect(
         borderRadius: const BorderRadius.only(
           topLeft: Radius.circular(20),
           topRight: Radius.circular(20),
         ),
+        child: Container(
+          color: Styles.cardColor, // Set the desired background color
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _buildNavItem(Icons.home, '', 0),
+              _buildNavItem(CupertinoIcons.search, '', 1),
+              _buildNavItem(CupertinoIcons.heart_fill, '', 2),
+              _buildNavItem(Icons.file_copy, '', 3),
+            ],
+          ),
+        ),
       ),
-      child: NavigationBar(
-        backgroundColor: Styles.darkColor,
-        indicatorColor: Styles.btnColor, // Set the indicator color here
-        selectedIndex: _tabController.index,
-        onDestinationSelected: (int index) {
-          setState(() {
-            _tabController.animateTo(index);
-          });
-        },
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home',
+    );
+  }
+
+  Widget _buildNavItem(IconData iconData, String label, int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _tabController.animateTo(index);
+        });
+      },
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 19),
+            child: Icon(
+              iconData,
+              size: 36,
+              color: _tabController.index == index ? Styles.btnColor : Styles.textColor,
+            ),
           ),
-          NavigationDestination(
-            icon: Icon(CupertinoIcons.search),
-            label: 'Search',
-          ),
-          NavigationDestination(
-            icon: Icon(CupertinoIcons.heart_fill),
-            label: 'Favorites',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.file_copy),
-            label: 'Transactions',
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              color: _tabController.index == index ? Styles.btnColor : Styles.textColor,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ],
       ),
